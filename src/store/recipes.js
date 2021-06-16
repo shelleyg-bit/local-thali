458c35c52bf8c889eab36b60255bb3a63212e0ff
Shelley Goel <shelley.goel109@gmail.com>
2021-06-16 13:52:32 -0700

    store: recipes: add more recipes

--- src/store/recipes.js
+++ src/store/recipes.js
@@ -1,76 +1,128 @@
 export default {
-  name: 'recipes',
-  state: {
-    recipesDb: [
-      {
-        location: 'Portland, OR, USA',
-        recipes: [
-          {
-            name: 'Potato onion soup',
-            ingredients: [{
-              name: 'potato',
-              category: 'produce',
-              quantity: 2,
-              units: 'x'
-            },
-            {
-              name: 'onion',
-              category: 'produce',
-              quantity: 2,
-              units: 'x'
-            },
-            {
-              name: 'salt',
-              category: 'spice',
-              quantity: 2,
-              units: 'tsp'
-            }],
-            steps: [
-              'boil & mash potatoes',
-              'finely chop onions',
-              'throw in a pot & boil'
-            ]
-          }
-        ]
-      },
-      {
-        location: 'Lucknow, UP, India',
-        recipes: [
-          {
-            name: 'aaloo dum banarsi',
-            ingredients: [{
-              name: 'potato',
-              category: 'produce',
-              quantity: 4,
-              units: 'x'
-            },
-            {
-              name: 'onion',
-              category: 'produce',
-              quantity: 3,
-              units: 'x'
-            },
-            {
-              name: 'salt',
-              category: 'spice',
-              quantity: 4,
-              units: 'tsp'
-            },
-            {
-              name: 'coriander powder',
-              category: 'spice',
-              quantity: 2,
-              units: 'tsp'
-            }],
-            steps: [
-              'boil & lightly fry potatoes',
-              'finely chop onions',
-              'throw in a pot & let it simmer'
-            ]
-          }
-        ]
-      }
-    ]
+	name: 'recipes',
+	state: {
+		recipesDb: [
+			{
+				location: 'Portland, OR, USA',
+				recipes: [
+					{
+						name: 'Potato onion soup',
+						ingredients: [{
+							name: 'potato',
+							category: 'produce',
+							quantity: 2,
+							units: 'x'
+						},
+						{
+							name: 'onion',
+							category: 'produce',
+							quantity: 2,
+							units: 'x'
+						},
+						{
+							name: 'salt',
+							category: 'spice',
+							quantity: 2,
+							units: 'tsp'
+						}],
+						steps: [
+							'boil & mash potatoes',
+							'finely chop onions',
+							'throw in a pot & boil'
+						]
+					},
+					{
+						name: 'French onion soup',
+						ingredients: [{
+							name: 'milk',
+							category: 'dairy',
+							quantity: 2,
+							units: 'cups'
+						},
+						{
+							name: 'onion',
+							category: 'produce',
+							quantity: 2,
+							units: 'x'
+						},
+						{
+							name: 'salt',
+							category: 'spice',
+							quantity: 2,
+							units: 'tsp'
+						}],
+						steps: [
+							'boil & mash potatoes',
+							'finely chop onions',
+							'throw in a pot & boil'
+						]
+					},
+					{
+						name: 'Leek onion soup',
+						ingredients: [{
+							name: 'Leeks',
+							category: 'produce',
+							quantity: 2,
+							units: 'x'
+						},
+						{
+							name: 'onion',
+							category: 'produce',
+							quantity: 2,
+							units: 'x'
+						},
+						{
+							name: 'salt',
+							category: 'spice',
+							quantity: 2,
+							units: 'tsp'
+						}],
+						steps: [
+							'boil & mash potatoes',
+							'finely chop onions',
+							'throw in a pot & boil'
+						]
+					}
+				]
+			},
+			{
+				location: 'Lucknow, UP, India',
+				recipes: [
+					{
+						name: 'aaloo dum banarsi',
+						ingredients: [{
+							name: 'potato',
+							category: 'produce',
+							quantity: 4,
+							units: 'x'
+						},
+						{
+							name: 'onion',
+							category: 'produce',
+							quantity: 3,
+							units: 'x'
+						},
+						{
+							name: 'salt',
+							category: 'spice',
+							quantity: 4,
+							units: 'tsp'
+						},
+						{
+							name: 'coriander powder',
+							category: 'spice',
+							quantity: 2,
+							units: 'tsp'
+						}],
+						steps: [
+							'boil & lightly fry potatoes',
+							'finely chop onions',
+							'throw in a pot & let it simmer'
+						]
+					}
+				]
+			}
+		]
 
-  }
+	}
 }
