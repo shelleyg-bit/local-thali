dd3716c8a228318b60c6313bef69f5f220912324
Shelley Goel <shelley.goel109@gmail.com>
2021-06-17 20:08:53 -0700

    thaliPlan: add page

---
+++ src/components/ThaliCard.vue
@@ -0,0 +1,21 @@
+<template>
+	<div class="q-pa-md row items-start q-gutter-md">
+		<!-- TODO: add card click show Recipe card -->
+    <q-card class="my-card">
+      <q-card-section>
+        {{ thaliName }}
+      </q-card-section>
+    </q-card>
+	</div>
+</template>
+<script>
+export default {
+	name: 'ThaliCard',
+	props: {
+		thaliName: {
+			type: String,
+			required: true
+		}
+	}
+}
+</script>
---
+++ src/pages/ThaliPlan.vue
@@ -0,0 +1,22 @@
+<template>
+<div>
+	<div class="text-h3 q-pa-md justify-center" > What's cooking? </div>
+<div class="row justify-evenly">
+<ThaliCard class="col-auto" v-for='thali in $store.state.user.plannedThalis'
+:key='thali' :thaliName="thali"/>
+
+</div>
+
+</div>
+	
+</template>
+<script>
+import ThaliCard from 'components/ThaliCard.vue'
+export default {
+	name: 'ThaliPlan',
+	components: {
+		ThaliCard
+	}
+}
+</script>
+
